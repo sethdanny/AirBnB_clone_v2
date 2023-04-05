7f29d5a974d6416e990d7eb85b332c3b8e2caed5
sethdanny <naddulidaniel1994@gmail.com>
2023-04-06 00:57:49 +0300

    fixed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,25 +1,22 @@
 #!/usr/bin/env bash
-# Script to configure new clean server and install nginx and restart the server
 
-sudo apt-get -y update
-sudo apt-get -y install nginx
-sudo mkdir /data/
-sudo mkdir /data/web_static/
-sudo mkdir /data/web_static/releases/
-sudo mkdir /data/web_static/shared/
-sudo mkdir /data/web_static/releases/test/
+# Script to configure a new clean server, install nginx and restart the server
 
-echo " <!Doctype html>
+sudo apt update
+sudo apt install -y nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "<!DOCTYPE html>
 <html>
   <head>
+    <title>Welcome to Holberton School</title>
   </head>
   <body>
-    Holberton School
+    <h1>Holberton School</h1>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data
-sudo sed -i '/server_name _;/a \ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/server_name _;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
-exit 0
