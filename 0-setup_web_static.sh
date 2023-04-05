2e186cef78537b43fd1e07d8e35993979baa0d67
sethdanny <naddulidaniel1994@gmail.com>
2023-04-05 22:16:14 +0300

    fix shellcheck error

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,7 +14,7 @@ echo " <!Doctype html>
   <head>
   </head>
   <body>
-    Best School
+    Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
@@ -22,3 +22,4 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 sudo sed -i '/server_name _;/a \ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
+exit 0
