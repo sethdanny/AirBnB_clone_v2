6111429e07bdfe6c2b46a66d8fb94fd6daf3dd9c
sethdanny <naddulidaniel1994@gmail.com>
2023-04-06 01:07:28 +0300

    more fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
-
 # Script to configure a new clean server, install nginx and restart the server
 
-sudo apt update
-sudo apt install -y nginx
+sudo apt-get update
+sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
+
 echo "<!DOCTYPE html>
 <html>
   <head>
-    <title>Welcome to Holberton School</title>
   </head>
   <body>
-    <h1>Holberton School</h1>
+    Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
 sudo chown -R ubuntu:ubuntu /data/
+
 sudo sed -i '/server_name _;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
