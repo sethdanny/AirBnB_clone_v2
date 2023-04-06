0674cd97879c09f5e8ef1cd9dec6b8a2dbe84b2e
sethdanny <naddulidaniel1994@gmail.com>
2023-04-06 15:46:24 +0300

    added tasks

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
-'''fabric script for task 2 0X03'''
+"""a script to send an archive file to a remote server
+and decompress it"""
 
-from fabric.api import put, run, env
-from os.path import exists
-
+from fabric.api import run, env, put
+import os.path
 
 env.hosts = ['35.229.54.225', '35.231.225.251']
-
+env.key_filename = "/home/seth/Desktop/alx-system_engineering-devops/0x0B-ssh/school"
+env.user = 'ubuntu'
 
 def do_deploy(archive_path):
-    '''distributes an archive to my web servers'''
-    if not exists(archive_path):
+    """a function to deploy code and decompress it"""
+    
+    if not os.path.isfile(archive_path):
         return False
+    compressed_file = archive_path.split("/")[-1]
+    no_extension = compressed_file.split(".")[0]
+    
     try:
-        file_name = archive_path.split("/")[-1].split(".")[0]
-        put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}".format(file_name))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(file_name, file_name))
-        run('rm /tmp/{}'.format(file_name))
-        run('mv /data/web_static/releases/{}/web_static/* '
-            '/data/web_static/releases/{}/'
-            .format(file_name, file_name))
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(file_name))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ '
-            '/data/web_static/current'.format(file_name))
-        return True
-    except Exception:
-        return False
+       remote_path = "/data/web_static/releases/{}/".format(no_extension)
+       sym_link = "/data/web_static/current"
+       put(archive_path, "/tmp/")
+       run("sudo mkdir - p {}".format(remote_path))
+       run("sudo tar -xvzf /tmp/{} -C {}".format(compressed_file, remote_path))
+       run("sudo rm /tmp/{}".format(compressed_file))
+       run("sudo ln -sf {} {}".format(remote_path, sym_link))
+       return True
+    except Exception as e:
+       return False
