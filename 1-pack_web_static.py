c086a444ec9387e3461836f5ba4de40e891287da
sethdanny <naddulidaniel1994@gmail.com>
2023-04-06 11:10:49 +0300

    changed import datetime

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,17 +5,20 @@ of my AirBnB webstatic directory
 
 
 from fabric.api import local
-import time
+from datetime import datetime
 
 
 def do_pack():
-    time_str = time.strftime("%Y%m%d%H%M%S")
+    """ function to create an archive """
+
+    time_now = datetime.now()
+    time_str = time_now.strftime("%Y%m%d%H%M%S")
 
     try:
         local("mkdir -p versions")
         local("tar -cvzf versions/web_static_{}.tgz web_static".format
               (time_str))
         return ("versions/web_static_{}".format(time_str))
-    
+
     except Exception as e:
         return None
