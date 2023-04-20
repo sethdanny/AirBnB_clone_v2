c80b4ad31fe264ca51130a478446fee23de0fd65
sethdanny <naddulidaniel1994@gmail.com>
2023-04-20 04:57:50 -0700

    add a new hbnb route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,6 +11,10 @@ def index():
     """ display Hello HBNB! """
     return "Hello HBNB!"
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnh():
+    """ display HBNB """
+    return "HBNB"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
