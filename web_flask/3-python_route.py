9633be505c5c0f46192d4a1683c5659a7edaa996
sethdanny <naddulidaniel1994@gmail.com>
2023-04-21 00:52:54 -0700

    fixed all

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -6,26 +6,31 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """ display Hello HBNB! """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnh():
     """ display HBNB """
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """ display C followed by a text given in a text variable"""
     return 'C ' + text.replace('_', ' ')
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
     """display “Python ”, followed by the value of the text variable"""
     return 'Python ' + text.replace('_', ' ')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -6,20 +6,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """ display Hello HBNB! """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnh():
     """ display HBNB """
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """ display C followed by a text given in a text variable"""
     return 'C ' + text.replace('_', ' ')
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -27,15 +31,18 @@ def pythoniscool(text='is cool'):
     """display “Python ”, followed by the value of the text variable"""
     return 'Python ' + text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """ display 'n is a number" if only n is an integer"""
     return "{:d} is a number".format(n)
 
+
 @app.route('/number_template/<int:n>', strict_slashes=True)
 def numbersandtemplate(n):
     """ display number using template """
     return render_template('5-number.html', n=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
